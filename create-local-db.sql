f0048d5169e328cbeda0091d4e1f61a5eaa5a07e
Moein Ghorbani <40246226+moindev65@users.noreply.github.com>
2023-06-30 20:44:10 +0330

    Add files via upload

---
+++ create-local-db.sql
@@ -0,0 +1,19 @@
+-- Local database definition.
+
+DROP DATABASE IF EXISTS local_db;
+
+CREATE DATABASE local_db;
+
+USE local_db;
+
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE weather (
+  id int(10) NOT NULL,
+  city varchar(30) NOT NULL DEFAULT '',
+  temperature int(10),
+  humidity varchar(30) NOT NULL DEFAULT '',
+  PRIMARY KEY (id)
+);
+
+INSERT INTO weather VALUES(1, 'Tehran',23,'69%');
